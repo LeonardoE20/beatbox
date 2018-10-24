f8f3b47c537ab66f82ca7fbef1c8d788023c0aff
LeonardoE20 <43798804+LeonardoE20@users.noreply.github.com>
2018-10-24 16:33:58 -0700

    add

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ $.getJSON('https://api.airtable.com/v0/appUPAzb0MxNy4F7A/Table%201?api_key=keyja
       var name = record.fields['Name'];
       var address = record.fields['Address'];
       var levels = record.fields['Levels'];
-      html.push(`<h2>${name}, ${address}, ${rating}</h2>`);
+      html.push(`<h2>${name}, ${address}, ${levels}</h2>`);
     });
     $('body').append(html);
   }
