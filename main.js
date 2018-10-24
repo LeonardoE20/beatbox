78b122c123b1554d45f44f53a2ab8d37709b384c
LeonardoE20 <43798804+LeonardoE20@users.noreply.github.com>
2018-10-24 16:00:39 -0700

    hh

---
+++ main.js
@@ -0,0 +1,15 @@
+$.getJSON('https://api.airtable.com/v0/appUPAzb0MxNy4F7A/Table%201?api_key=keyjafBekvjEMp3cm',
+  function(airtable){
+    var html = [];
+    $.each(airtable.records, function(index, record) {
+      var name = record.fields['Name'];
+      var address = record.fields['Address'];
+      var rating = record.fields['Rating'];
+      html.push(`<h2>${name}, ${address}, ${rating}</h2>`);
+    });
+    $('body').append(html);
+  }
+);
+
+
+
