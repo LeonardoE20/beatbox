ce5c6b10dfa89d8102bff0bd8bdf2481f47481fc
LeonardoE20 <43798804+LeonardoE20@users.noreply.github.com>
2018-12-01 13:55:56 -0800

    change

--- main.js
+++ main.js
@@ -2,62 +2,67 @@ function getParameterByName(name, url) {
   if (!url) url = window.location.href;
   name = name.replace(/[\[\]]/g, '\\$&');
   var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-      results = regex.exec(url);
+    results = regex.exec(url);
   if (!results) return null;
   if (!results[2]) return '';
   return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
-var getAllRecords = function() {
-$.getJSON('https://api.airtable.com/v0/appUPAzb0MxNy4F7A/Table%201?api_key=keyjafBekvjEMp3cm',
-  function(airtable){
-    var html = [];
-    $.each(airtable.records, function(index, record) {
-      var name = record.fields['Name'];
-      var description = record.fields['Description'];
-      var levels = record.fields['Levels'];
-      var learning = record.fields['Learning'];
-      html.push(`<button type="button" class="btn btn-primary">${name}</button>`);
-    });
-    $('body').append(html);
-  }
-);
-}
-
-style="width: 100%; height: 500px;"
-
-var getOneRecord = function(id) {
-  $.getJSON(`https://api.airtable.com/v0/appSrgke7E0ElZhMY/Locations/${id}?api_key=key2m8VgwGT2iztad`,
-    function(record){
+var getAllRecords = function () {
+  $.getJSON('https://api.airtable.com/v0/appUPAzb0MxNy4F7A/BEATBOX%20TUTORIALS?api_key=keyjafBekvjEMp3cm',
+    function (airtable) {
       var html = [];
-      var name = record.fields['Name'];
-      var address = record.fields['Address'];
-      var rating = record.fields['Rating'];
-      var picture = record.fields['Pictures'];
-      var cost = record.fields['Cost'];
-      var type = record.fields['Type'];
-      html.push(detailView(name, address, rating, picture, cost, type ));
-      $('body').append(html);
+      $.each(airtable.records, function (index, record) {
+       var id = record.id;
+        var name = record.fields['Name'];
+        var description = record.fields['Description'];
+        var pictures = record.fields['Pictures'];
+        var videos = record.fields['Videos']; 
+        var levels = record.fields['Levels'];
+        var learning = record.fields['Learning'];
+        html.push(listView(id, name, description, pictures, videos, levels, learning));
+      });
+        $('body').append(html);
     }
   );
 }
 
-var listView = function(id, name, rating, picture) {
+var getOneRecord = function (id) {
+  $.getJSON(`https://api.airtable.com/v0/appUPAzb0MxNy4F7A/BEATBOX%20TUTORIALS/${id}?api_key=keyjafBekvjEMp3cm`,
+    function (record) {
+      var html = [];
+      var name = record.fields['Name'];
+      var description = record.fields['Description'];
+      var pictures = record.fields['Pictures'];
+      var videos = record.fields['Videos'];
+      var levels = record.fields['Levels'];
+      var learning = record.fields['Learning'];
+      html.push(detailView(name, description, pictures, videos, levels, learning));
+    $('body').append(html);
+    }
+  );
+}
+// ${videos ? `<iframe width="560" height="315" src="${videos[0].url}" frameborder="0" 
+//   allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>` : ``}
+
+var listView = function (id, name, description, pictures, videos, levels, learning) {
   return `
-    <h2><a href="index.html?id=${id}">${name}</a></h2>
-    <p>${rating}</p>
-    ${picture ? `<img src="${picture[0].url}">` : ``}
+  <h2>${name}</h2>
+  <h2><a href="index.html?id=${id}">${description}</a></h2>
+  <iframe width="560" height="315" src=${videos}> frameborder="0" 
+  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    ${levels ? `<img src="${levels[0].url}">` : ``}
+    <p>${learning}</p>
   `;
 }
 
-var detailView = function(name, address, rating, picture, cost, type) {
+var detailView = function (name, description, pictures, videos, levels, learning) {
   return `
-    <h2>${name}</h2>
-    <p>${address}</p>
-    <p>${rating}</p>
-    <p>${cost}</p>
-    <p>${type}</p>
-    ${picture ? `<img src="${picture[0].url}">` : ``}
+  <h2><a href="index.html?id=${name}">${description}</a></h2>
+  ${pictures ? `<img src="${pictures[0].url}">` : ``}
+  <p>${videos}</p>
+  ${levels ? `<img src="${levels[0].url}">` : ``}
+  <p>${learning}</p>
   `;
 }
 
